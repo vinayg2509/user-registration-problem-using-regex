fdfb15c0b78e13a0df289a865f2b3590ad91587f
vinayg2509 <vinayg2509@gmail.com>
2025-05-28 11:24:03 +0530

    [Vinay]Add regex for validating email format with optional and mandatory parts

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -4,24 +4,41 @@ import java.util.Scanner;
 
 public class UserRegistration
 {
+    //Method to validate first name
     public static boolean validateFirstName(String firstName)
     {
         return firstName.matches("^[A-Z][a-zA-Z]{2,}$");
     }
+
+    //Method to validate second name
     public static boolean validateSecondName(String secondName)
     {
         return secondName.matches("^[A-Z][a-zA-Z]{2,}$");
     }
+
+    //Method to validate email
+    public static  boolean validateEmail(String email)
+    {
+        return  email.matches("^[a-zA-Z0-9]+([._+-]+[a-zA-Z0-9])?@[a-zA-Z0-9]+[a-zA-Z0-9]+\\.[a-z]{2,4}(\\.[a-z]{2,})?$");
+      //return email.matches("^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)?@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$");
+        //return email.matches("^[a-zA-Z0-9]+([._%+-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\\.[a-zA-Z]{2,})+$");
+    }
+
     public static void main(String[] args)
     {
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter First Name");
-        String firstName=scanner.nextLine();
-        System.out.println("Enter Second Name");
-        String secondName=scanner.nextLine();
+//        System.out.println("Enter First Name");
+//        String firstName=scanner.nextLine();
+//        System.out.println("First name having First character Upper and having minium 3 character "+validateFirstName(firstName));
 
-        System.out.println("First name having First character Upper and having minium 3 character "+validateFirstName(firstName));
-        System.out.println("Second name having First character Upper and having minium 3 character "+validateSecondName(secondName));
+//        System.out.println("Enter Second Name");
+//        String secondName=scanner.nextLine();
+//        System.out.println("Second name having First character Upper and having minium 3 character "+validateSecondName(secondName));
+
+        System.out.println("Enter email");
+        String email=scanner.nextLine();
+        System.out.println("Email validate "+validateEmail(email));
+
 
     }
 
