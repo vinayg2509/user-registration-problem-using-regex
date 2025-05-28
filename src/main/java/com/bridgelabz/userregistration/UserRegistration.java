af657af22fee64d4c9d47b75bf302c709cda6c29
vinayg2509 <vinayg2509@gmail.com>
2025-05-28 09:55:19 +0530

    [Vinay]Add regex for validating last name starting with uppercase and min 3 characters

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -8,12 +8,22 @@ public class UserRegistration
     {
         return firstName.matches("^[A-Z][a-zA-Z]{2,}$");
     }
-    public static void main(String[] args) {
+    public static boolean validateSecondName(String secondName)
+    {
+        return secondName.matches("^[A-Z][a-zA-Z]{2,}$");
+    }
+    public static void main(String[] args)
+    {
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter First Name");
         String firstName=scanner.nextLine();
+        System.out.println("Enter Second Name");
+        String secondName=scanner.nextLine();
 
         System.out.println("First name having First character Upper and having minium 3 character "+validateFirstName(firstName));
+        System.out.println("Second name having First character Upper and having minium 3 character "+validateSecondName(secondName));
 
     }
+
+
 }
